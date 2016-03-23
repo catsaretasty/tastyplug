1045d4e4d57a3a0390549011c780faf88c24433e
chip <chip@chip.sh>
2016-03-23 16:50:53 -0700

    Change license to GPLv3+
    From: Olivier Houle (fungus)
    To: Chip (git)
    Date: Wed, 23 Mar 2016 16:28:12 -0700
    Subject: TastyPlug Licensing
    
    I author TastyPlug to be published under GPLv3+.
    
    -fungus

--- tastyplug.js
+++ tastyplug.js
@@ -1,8 +1,23 @@
 /*
- Copyright (c) 2013-2015 by Olivier Houle (Fungus)
- Legacy Chat option made by Git!
- Tastymojis by Nackloose and Fungus.
- Please do not copy or modify without my permission.
+ * TastyPlug, a plug.dj extension
+ * Original work Copyright (c) 2013-2015 by Olivier Houle (Fungus)
+ * Legacy Chat option made by Git! Tastymojis by Nackloose and Fungus.
+ * Please do not copy or modify without my (Oliver Houle's) permission.
+ *
+ * Modified work Copyright (C) 2016 Chip (git)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 Array.prototype.isArray = true;
 var tastyPlugShutDown;
